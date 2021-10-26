231154aad699763a95c10e1d0f18842d853fa236
DreaM7625 <sebastian.galan7625@gmail.com>
2021-10-26 15:19:06 +0200

    Zadania Lab2

---
+++ Lab2/Zad3/cmake-build-debug/CMakeFiles/Zad3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zad3.dir/main.cpp.obj"
+  "Zad3.exe"
+  "Zad3.exe.manifest"
+  "Zad3.lib"
+  "Zad3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zad3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab2/Zad4/cmake-build-debug/CMakeFiles/Zad4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zad4.dir/main.cpp.obj"
+  "Zad4.exe"
+  "Zad4.exe.manifest"
+  "Zad4.lib"
+  "Zad4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zad4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
